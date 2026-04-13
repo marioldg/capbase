a41cc3b965ec06f03c2164874043358aa3fd3a10
marioldg <matuteldgmario@gmail.com>
2026-04-13 18:44:11 +0200

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/capbase/capbase/CapbaseApplication.java
@@ -0,0 +1,13 @@
+package com.capbase.capbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capbase/capbase/CapbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capbase.capbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
