e64ae4f59553328fa118010e38465ae61066ece4
marioldg <matuteldgmario@gmail.com>
2026-04-14 12:02:22 +0200

    Añade validacion de categoria con @NotBlank y @Valid

--- src/main/java/com/capbase/capbase/controller/CategoriaController.java
+++ src/main/java/com/capbase/capbase/controller/CategoriaController.java
@@ -2,6 +2,7 @@ package com.capbase.capbase.controller;
 
 import com.capbase.capbase.model.Categoria;
 import com.capbase.capbase.service.CategoriaService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,12 +23,14 @@ public class CategoriaController {
     }
 
     @PostMapping
-    public Categoria guardarCategoria(@RequestBody Categoria categoria) {
+    public Categoria guardarCategoria(@Valid @RequestBody Categoria categoria) {
+        // Aqui valido que la categoria que llega cumple las reglas (por ejemplo, nombre no vacio)
         return categoriaService.guardarCategoria(categoria);
     }
 
     @PutMapping("/{id}")
-    public Categoria actualizarCategoria(@PathVariable Long id, @RequestBody Categoria categoria) {
+    public Categoria actualizarCategoria(@PathVariable Long id, @Valid @RequestBody Categoria categoria) {
+        // Tambien valido en el update para evitar datos incorrectos
         return categoriaService.actualizarCategoria(id, categoria);
     }
 
--- src/main/java/com/capbase/capbase/model/Categoria.java
+++ src/main/java/com/capbase/capbase/model/Categoria.java
@@ -1,6 +1,7 @@
 package com.capbase.capbase.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -14,6 +15,8 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // El nombre es obligatorio, no quiero categorias vacias o sin sentido
+    @NotBlank(message = "El nombre de la categoria es obligatorio")
     private String nombre;
 
     private String descripcion;
