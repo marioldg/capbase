c2f4906b8da8c2010358eba9e39b0f0a6d2896ec
marioldg <matuteldgmario@gmail.com>
2026-04-21 19:20:58 +0200

    refactor: reorganizar proyecto a estructura backend/frontend

--- src/main/java/com/capbase/capbase/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capbase.capbase.repository;
-
-import com.capbase.capbase.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- src/main/java/com/capbase/capbase/repository/PresupuestoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capbase.capbase.repository;
-
-import com.capbase.capbase.model.Presupuesto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PresupuestoRepository extends JpaRepository<Presupuesto, Long> {
-}
